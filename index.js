6de906c74d8734a2cdcc3d8b7a1eb67db8d49ee3
SCARLET-SARAH <162133775+SCARLET-SARAH@users.noreply.github.com>
2024-03-19 23:41:55 +0300

    create arrows.js

---
+++ index.js
@@ -0,0 +1,18 @@
+it("divide divides 2000 by 100", () => {
+    let a = 2000
+    let b = 100
+    (divide(a,b)).to.equal(a/b)
+  })
+console.log(a/b)
+it("has an arrow function called square", () => {
+    expect(square).to.exist
+  })
+  it("has an arrow function called add", () => {
+    expect(add).to.exist
+  })
+  it("add arrow function takes two parameters and adds them together", () => {
+    let a = 3
+    let b = 4
+    expect(add(a,b)).to.equal(a+b)
+  })
+
